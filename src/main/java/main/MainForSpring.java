4a61041c1e7db332bd75527f03a68616cf108051
user <user@COM-2-204-13>
2019-03-28 15:59:54 +0900

    다겠돌 리머 내

--- src/main/java/main/MainForSpring.java
+++ src/main/java/main/MainForSpring.java
@@ -16,7 +16,9 @@ import spring.MemberListPrinter;
 import spring.MemberNotFoundException;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
+import spring.VersionPrinter;
 import spring.WrongIdPasswordException;
+import spring.MemberPrinter;
 
 public class MainForSpring {
 	
@@ -46,6 +48,9 @@ public class MainForSpring {
 				continue;
 			}else if(command.startsWith("info ")) {
 				processInfoCommand(command.split(" "));
+				continue;
+			}else if(command.equals("version")) {
+				processVersionCommand();
 				continue;
 			}
 			printHelp();
@@ -121,5 +126,11 @@ public class MainForSpring {
 		MemberInfoPrinter infoPrinter =
 				ctx.getBean("infoPrinter", MemberInfoPrinter.class);
 		infoPrinter.printMemberInfo(arg[1]);
+	}
+	
+	private static void processVersionCommand() {
+		VersionPrinter versionPrinter =
+				ctx.getBean("versionPrinter", VersionPrinter.class);
+		versionPrinter.print();
 	}
 }
